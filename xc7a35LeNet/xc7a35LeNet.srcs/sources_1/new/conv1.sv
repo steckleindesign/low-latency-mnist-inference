062afa88e2b2adbb2e787b2f846525e3a84ec16a
steckleindesign <steckleindesign@gmail.com>
2024-11-23 23:58:13 -0500

    write up control signals for a more serial design

--- xc7a35LeNet/xc7a35LeNet.srcs/sources_1/new/conv1.sv
+++ xc7a35LeNet/xc7a35LeNet.srcs/sources_1/new/conv1.sv
@@ -19,8 +19,60 @@ module conv1 #(
     output logic signed [15:0] o_feature_map[NUM_FILTERS-1:0][IMAGE_HEIGHT-FILTER_SIZE:0][IMAGE_WIDTH-FILTER_SIZE:0]
 );
 
+    // For height=5 filter, we only need to store 4 rows of pixel data
+    // We could reduce latency if we get creative with bringing in data
+    logic [7:0] line_buffer[FILTER_SIZE-2:0][IMAGE_WIDTH-1:0];
     
+    logic [7:0] window[FILTER_SIZE-1][FILTER_SIZE-1];
+    
+    // control counters
+    logic [$clog2(IMAGE_HEIGHT)-1:0] row_ctr;
+    logic [$clog2(IMAGE_WIDTH)-1:0]  col_ctr;
+    logic [$clog2(NUM_FILTERS)-1:0]  filter_ctr;
+    
+    logic signed [7:0] filter_weights[NUM_FILTERS-1:0][FILTER_SIZE*FILTER_SIZE-1];
+    
+    logic signed mac_accumulator;
+    
+    logic [$clog2(FILTER_SIZE*FILTER_SIZE)-1:0] mac_counter;
+    
+    typedef enum logic [1:0] {
+        IDLE,
+        LOAD_WINDOW,
+        MACC,
+        DATA_OUT
+    } state;
+    state next_state, curr_state;
+    
+    logic pixel_valid;
+    logic window_valid;
+    logic mac_done;
+    
+    // Can add a reset later
+    // use proper next state practice here
+    always_ff @(posedge clk)
+    begin
+        curr_state <= next_state;
+    end
+    
+    always_comb begin
+        case (curr_state)
+            IDLE: begin
+                next_state = IDLE;
+            end
+            LOAD_WINDOW: begin
+                next_state = COMPUTE;
+            end
+            COMPUTE: begin
+                next_state = DATA_OUT;
+            end
+            DATA_OUT: begin
+                next_state = COMPUTE;
+            end
+        endcase
+    end
 
+    
 
 /* Parallel implementation
 
