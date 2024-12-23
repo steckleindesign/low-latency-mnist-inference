6e68b0917455fde589fe2c9716f96b9ba1f7a89a
steckleindesign <steckleindesign@gmail.com>
2024-12-23 02:17:30 -0500

    additonal adder tree result stage, valid signals, feature out port depth

--- xc7a35LeNet/xc7a35LeNet.srcs/sources_1/new/conv1.sv
+++ xc7a35LeNet/xc7a35LeNet.srcs/sources_1/new/conv1.sv
@@ -1,11 +1,8 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 /*
-    TODO: Determine how to fit in the +bias operation
-            -See if we can do it in DSP48 without hurting max clock frequency
-            -See the max clock frequency penalty when implemented in fabric
           
-          90 DSPS, 50 BRAMS (36Kb each)
+          90 DSPs, 50 BRAMS (36Kb each)
           6 filters for conv1, 5x5 filter (25 * ops), 28x28 conv ops (784)
           = 6*(5*5)*(28*28) = 117600 * ops / 90 DSPs = 1306.6 = 1307 cycs theoretically
           
@@ -14,13 +11,11 @@
           Will take us 46 clock cycles per row this way => (6*28*5*5 - 6*5*2) / 90 = 46
           Note that there are still improvements to be made, still many static 0's on
           the edges that we waste MACC (DSP) operations on
+          Need to decide how to handle max pooling since input features will be 90,
+          which is not a multiple of 4
           
-          // Accumulation architecture (adder tree) - research additional adder structures
-          // 5*5=25 accumulations per output feature, but only 15 * operations for
-          // a given output feature execute in a single clock cycle.
-          // 15 DSP outputs will take clog2(15)=4 clock cycles to add together.
-          // For each feature out, there will be a latency of 7 clock cycles (computed on paper notes)
-          // Study how to get outputs of DSP48s to carry chain resources efficiently
+          Accumulation architecture is currently adder tree - research additional adder structures
+          Study how to get outputs of DSP48s to carry chain resources efficiently
           
 */
 //////////////////////////////////////////////////////////////////////////////////
@@ -41,7 +36,7 @@ module conv1 #(
     // No CDC needed, a new feature is available each clock cycle
     input  logic         [7:0] i_feature,
     output logic               o_feature_valid,
-    output logic signed [15:0] o_feature,
+    output logic signed [15:0] o_features[NUM_FILTERS-1:0],
     // Letting pixel RAM know we can't take in any data
     output logic               o_buffer_full
 );
@@ -99,21 +94,23 @@ module conv1 #(
     logic signed [23:0] adder1_stage4[NUM_FILTERS-1:0][4:0];  // 5 adder outs from stage 3
     logic signed [23:0] adder1_stage5[NUM_FILTERS-1:0][2:0];  // 3 adder outs from stage 4
     logic signed [23:0] adder1_stage6[NUM_FILTERS-1:0][1:0];  // 2 adder outs from stage 5
+    logic signed [23:0] adder1_result[NUM_FILTERS-1:0];       // adder tree 1 result
     logic signed [23:0] adder2_stage1[NUM_FILTERS-1:0][4:0];  // 5 dsp outs
     logic signed [23:0] adder2_stage2[NUM_FILTERS-1:0][17:0]; // 3 adder outs from stage 1 + 15 dsp outs
     logic signed [23:0] adder2_stage3[NUM_FILTERS-1:0][13:0]; // 9 adder outs from stage 2 + 5 dsp outs
     logic signed [23:0] adder2_stage4[NUM_FILTERS-1:0][6:0];  // 7 adder outs from stage 3
     logic signed [23:0] adder2_stage5[NUM_FILTERS-1:0][3:0];  // 4 adder outs from stage 4
     logic signed [23:0] adder2_stage6[NUM_FILTERS-1:0][1:0];  // 2 adder outs from stage 5
+    logic signed [23:0] adder2_result[NUM_FILTERS-1:0];       // adder tree 2 result
     logic signed [23:0] adder3_stage1[NUM_FILTERS-1:0][9:0];  // 10 dsp outs
     logic signed [23:0] adder3_stage2[NUM_FILTERS-1:0][19:0]; // 5 adder outs from stage 1 + 15 dsp outs
     logic signed [23:0] adder3_stage3[NUM_FILTERS-1:0][9:0];  // 10 adder outs from stage 2
     logic signed [23:0] adder3_stage4[NUM_FILTERS-1:0][4:0];  // 5 adder outs from stage 3
     logic signed [23:0] adder3_stage5[NUM_FILTERS-1:0][2:0];  // 3 adder outs from stage 4
     logic signed [23:0] adder3_stage6[NUM_FILTERS-1:0][1:0];  // 2 adder outs from stage 5
-    
-    // Is 16-wide ok?
-    logic signed [15:0] macc_accum[NUM_FILTERS-1:0];
+    logic signed [23:0] adder3_result[NUM_FILTERS-1:0];       // adder tree 3 result
+    // Wire to features output port
+    logic signed [23:0] macc_acc[NUM_FILTERS-1:0];
     // Register outputs of DSPs
     // TODO: Flatten
     logic signed [23:0] mult_out[NUM_FILTERS-1:0][FILTER_SIZE*3-1:0];
@@ -165,34 +162,12 @@ module conv1 #(
             for (int j = 0; j < 5; j++)
                 for (int k = 0; k < 3; k++)
                     mult_out[i][k*5+j] <= weight_operands[i][j][k] * feature_operands[j][k];
-    
-    //    always_ff @(posedge i_clk) begin
-    //        if (macc_en) begin
-    //            case(state)
-    //                ONE:
-    //                    // 15 -> adder tree 1
-    //                TWO:
-    //                    // 10 -> adder tree 1,
-    //                    // 5  -> adder tree 2
-    //                THREE:
-    //                    // 15 -> adder tree 2
-    //                FOUR:
-    //                    // 5  -> adder tree 2
-    //                    // 10 -> adder tree 3
-    //                FIVE:
-    //                    // 15 -> adder tree 3
-    //            endcase
-    //        end
-    //        adder_tree_valid_sr <= { adder_tree_valid_sr[0][5:0], state == ONE  };
-    //        adder_tree_valid_sr <= { adder_tree_valid_sr[1][5:0], state == TWO  };
-    //        adder_tree_valid_sr <= { adder_tree_valid_sr[2][5:0], state == FOUR };
-    //    end
 
     // Only on MACC enable, not on first feature valid signal
     // This is because the very first clock cycle after valid data should only enable the DSP operation,
     // not the adder tree logic
     // (TODO: try to really understand clock enables vs. gating vs. if the macc_en is just treated as a logic variable)
-    // TODO: adder tree valid signals
+    // TODO: add bias to front of tree
     always_ff @(posedge i_clk) begin
         if (macc_en) begin
             for (int i = 0; i < NUM_FILTERS; i++) begin
@@ -222,6 +197,8 @@ module conv1 #(
                 adder1_stage6[i][1]     <= adder1_stage5[i][2];
                 adder1_stage6[i][0]     <= adder1_stage5[i][0] + adder1_stage5[i][1];
                 
+                adder1_result[i]        <= adder1_stage6[i][1] + adder1_stage6[i][0];
+                
                 // Adder tree structure 2
                 adder2_stage1[i]        <= mult_out[i][14:10];
                 
@@ -234,27 +211,29 @@ module conv1 #(
                 
                 for (int j = 0; j < 9; j++)
                     adder2_stage3[i][j+5] <= adder2_stage2[i][j*2] + adder2_stage2[i][j*2+1];
-                adder2_stage3[i][4:0]      <= mult_out[i][4:0];
+                adder2_stage3[i][4:0]   <= mult_out[i][4:0];
                 
                 for (int j = 0; j < 7; j++)
                     adder2_stage4[i][j+5] <= adder2_stage3[i][j*2] + adder2_stage3[i][j*2+1];
                     
-                adder2_stage5[i][3]        <= adder2_stage4[i][6];
+                adder2_stage5[i][3]     <= adder2_stage4[i][6];
                 for (int j = 0; j < 3; j++)
                     adder2_stage5[i][j] <= adder2_stage4[i][j*2] + adder2_stage4[i][j*2+1];
                     
                 for (int j = 0; j < 2; j++)
                     adder2_stage6[i][j+5] <= adder2_stage5[i][j*2] + adder2_stage5[i][j*2+1];
-                    
+                
+                adder2_result[i]        <= adder2_stage6[i][1] + adder2_stage6[i][0];
+                
                 // Adder tree structure 3
-                adder3_stage1[i][9:5]      <= mult_out[i][14:10];
-                adder3_stage1[i][4:0]      <= mult_out[i][9:5];
+                adder3_stage1[i][9:5]   <= mult_out[i][14:10];
+                adder3_stage1[i][4:0]   <= mult_out[i][9:5];
                 
                 for (int j = 0; j < 5; j++)
                     adder3_stage2[i][j+15] <= adder3_stage1[i][j*2] + adder3_stage1[i][j*2+1];
-                adder3_stage2[i][14:10]    <= mult_out[i][14:10];
-                adder3_stage2[i][9:5]      <= mult_out[i][9:5];
-                adder3_stage2[i][4:0]      <= mult_out[i][4:0];
+                adder3_stage2[i][14:10] <= mult_out[i][14:10];
+                adder3_stage2[i][9:5]   <= mult_out[i][9:5];
+                adder3_stage2[i][4:0]   <= mult_out[i][4:0];
                 
                 for (int j = 0; j < 10; j++)
                     adder3_stage3[i][j] <= adder3_stage2[i][j*2] + adder3_stage2[i][j*2+1];
@@ -267,12 +246,27 @@ module conv1 #(
                 for (int j = 0; j < 2; j++)
                     adder3_stage5[i][j] <= adder3_stage4[i][j*2] + adder3_stage4[i][j*2+1];
                     
-                adder3_stage6[i][1] <= adder3_stage5[i][2];
-                adder3_stage6[i][0] <= adder3_stage5[i][0] + adder3_stage5[i][1];
+                adder3_stage6[i][1]     <= adder3_stage5[i][2];
+                adder3_stage6[i][0]     <= adder3_stage5[i][0] + adder3_stage5[i][1];
+                
+                adder3_result[i]        <= adder3_stage6[i][1] + adder3_stage6[i][0];
             end
         end
     end
     
+    always_comb
+        // Would casex block be better here?
+        if (adder_tree_valid_sr[0][6])
+            macc_acc <= adder1_result;
+        else if (adder_tree_valid_sr[1][6])
+            macc_acc <= adder2_result;
+        else if (adder_tree_valid_sr[2][6])
+            macc_acc <= adder3_result;
+        else
+            macc_acc <= macc_acc;
+    
+    // Do we want to use an enable here?
+    // Maybe could K-map this down to less lines, but the verbose version is more understandable
     always_comb begin
         case(state)
             ONE: begin
@@ -337,16 +331,20 @@ module conv1 #(
         if (macc_en) begin
             case(state)
                 ONE: begin
-                    
+                    // 15 -> adder tree 1
                 end
                 TWO: begin
+                    // 10 -> adder tree 1,
+                    // 5  -> adder tree 2
                     feat_col_ctr <= feat_col_ctr + 1;
                     
                 end
                 THREE: begin
-                    
+                    // 15 -> adder tree 2
                 end
                 FOUR: begin
+                    // 5  -> adder tree 2
+                    // 10 -> adder tree 3
                     if (feat_col_ctr == COL_END) begin
                         feat_row_ctr <= feat_row_ctr + 1;
                         feat_col_ctr <= 0;
@@ -354,10 +352,14 @@ module conv1 #(
                         feat_col_ctr <= feat_col_ctr + 1;
                 end
                 FIVE: begin
+                    // 15 -> adder tree 3
                     feat_col_ctr <= feat_col_ctr + 1;
                     
                 end
             endcase
+            adder_tree_valid_sr[0] <= { adder_tree_valid_sr[0][5:0], state == ONE  };
+            adder_tree_valid_sr[1] <= { adder_tree_valid_sr[1][5:0], state == TWO  };
+            adder_tree_valid_sr[2] <= { adder_tree_valid_sr[2][5:0], state == FOUR };
         end
     end
     
@@ -394,8 +396,6 @@ module conv1 #(
                     line_buffer         <= '{default: 0};
                     adder_tree_valid_sr <= '{default: 0};
                     macc_en             <= 1;
-                    for (int i = 0; i < NUM_FILTERS; i++)
-                        macc_accum[i] <= biases[i];
                         
                     // Use logic, instead of setting output directly
                     o_feature_valid <= 0;
@@ -405,9 +405,10 @@ module conv1 #(
     end
     
     // How many features do we want to output in parallel?
-    // assign o_feature
-    // assign o_feature_valid
-    
-    assign o_buffer_full = lb_full;
+    assign o_features      = macc_acc;
+    assign o_feature_valid = adder_tree_valid_sr[0][6] ||
+                             adder_tree_valid_sr[1][6] ||
+                             adder_tree_valid_sr[2][6];
+    assign o_buffer_full   = lb_full;
 
 endmodule
