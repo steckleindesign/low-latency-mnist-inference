19db0ba1c6fec9cf7f212773853e01157d1032b4
steckleindesign <steckleindesign@gmail.com>
2024-12-28 23:48:21 -0500

    study and document trainable param/connection count, latency w full DSP48 util, for conv2

--- xc7a35LeNet/xc7a35LeNet.srcs/sources_1/new/conv.sv
+++ xc7a35LeNet/xc7a35LeNet.srcs/sources_1/new/conv.sv
@@ -1,6 +1,38 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 
+/*
+    Connections in the 2nd convolutional layer are not as trivial
+    In the prior layer, S2, the first max pooling layer, there are 6x14x14 feature maps
+    In this convolutional layer, there are 16 feature maps. The feature maps of S2 are
+    tied to each map of this convolutional layer, C3, are as follows:
+    Map 0:  0, 1, 2
+    Map 1:  1, 2, 3
+    Map 2:  2, 3, 4
+    Map 3:  3, 4, 5
+    Map 4:  0, 4, 5
+    Map 5:  0, 1, 5
+    Map 6:  0, 1, 2, 3
+    Map 7:  1, 2, 3, 4
+    Map 8:  2, 3, 4, 5
+    Map 9:  0, 3, 4, 5
+    Map 10: 0, 1, 4, 5
+    Map 11: 0, 1, 2, 5
+    Map 12: 0, 1, 3, 4
+    Map 13: 1, 2, 4, 5
+    Map 14: 0, 2, 3, 5
+    Map 15: 0, 1, 2, 3, 4, 5
+    
+    There are 6*(3*5*5 + 1) + 9*(4*5*5 + 1) + 6*5*5 + 1 = 1516 trainable parameters
+    
+    Total * operations = 6*10*10*5*5*3 + 9*10*10*5*5*4 + 10*10*5*5*6 = 10*10*(1516-16) = 150000
+    
+    The Artix-7 35 device has 90 DSP48s.
+    We will have all 90 DSPs available as with the current architecture, all 90 DSPs will be free for conv2.
+    150000/90 = 1666.67 = 1667 clock cycles worth of full DSP48 utilization.
+    
+*/
+
 //////////////////////////////////////////////////////////////////////////////////
 
 module conv #(
