f84f9f6454901987bb1e5a6c09cff79c22ccef22
steckleindesign <steckleindesign@gmail.com>
2024-12-29 23:43:18 -0500

    write out conv2 adder tree structures, conv2 weights/weight file structure

--- xc7a35LeNet/xc7a35LeNet.srcs/sources_1/new/conv.sv
+++ xc7a35LeNet/xc7a35LeNet.srcs/sources_1/new/conv.sv
@@ -31,17 +31,93 @@
     We will have all 90 DSPs available as with the current architecture, all 90 DSPs will be free for conv2.
     150000/90 = 1666.67 = 1667 clock cycles worth of full DSP48 utilization.
     
+    DSP distribution:
+    First process the 3*5*5 maps (6 maps, 90/6 = 15 DSP48s per map)
+    second process the 4*5*5 maps (9 maps, 90/9 = 10 DSP48s per map),
+    third process the 6*5*5 map (1 map, 90 DSPs used for this map)
+    
+    Adder tree structures:
+    3*5*5 maps
+        15
+        8 + 15
+        12 + 15
+        14 + 15
+        15 + 15
+        15
+        8
+        4
+        2
+        1
+    
+    4*5*5 maps
+        10
+        5 + 10
+        8 + 10
+        9 + 10
+        10 + 10
+        10 + 10
+        10 + 10
+        10 + 10
+        10 + 10
+        10 + 10
+        10
+        5
+        3
+        2
+        1
+        
+    6*5*5 map (3 adder tree structures here, because 90 (DSPs) is not a factor of the # of * ops per feature (150)
+    Tree structure 1
+        90
+        45 + 60
+        53
+        27
+        14
+        7
+        4
+        2
+        1
+        
+    Tree structure 2
+        30
+        15 + 90
+        53 + 30
+        42
+        21
+        11
+        6
+        3
+        2
+        1
+        
+    Tree structure 3
+        60
+        30 + 90
+        60
+        30
+        15
+        8
+        4
+        2
+        1
+        
+    We might need to be smarter, lots of unique adder tree structures across the 2 conv layers alone
+    
+    FSMs:
+    
 */
 
 //////////////////////////////////////////////////////////////////////////////////
 
 module conv #(
-    parameter string WEIGHTS_FILE = "weights.mem",
-    parameter string BIASES_FILE  = "biases.mem",
-    parameter        INPUT_WIDTH  = 32,
-    parameter        INPUT_HEIGHT = 32,
-    parameter        FILTER_SIZE  = 5,
-    parameter        NUM_FILTERS  = 16
+    parameter string WEIGHTS_FILE3MAP = "weights3map.mem",
+    parameter string WEIGHTS_FILE4MAP = "weights4map.mem",
+    parameter string WEIGHTS_FILE6MAP = "weights6map.mem",
+    parameter string BIASES_FILE      = "biases.mem",
+    parameter        INPUT_WIDTH      = 14,
+    parameter        INPUT_HEIGHT     = 14,
+    parameter        FILTER_SIZE      = 5,
+    parameter        NUM_FILTERS      = 16
 ) (
     input  logic               i_clk,
     input  logic               i_rst,
@@ -51,144 +127,30 @@ module conv #(
     output logic signed [15:0] o_features[NUM_FILTERS-1:0]
 );
 
-    // Computed local params from module parameters
     localparam WINDOW_AREA   = FILTER_SIZE * FILTER_SIZE;
     localparam OUTPUT_HEIGHT = INPUT_HEIGHT - FILTER_SIZE + 1;
     localparam OUTPUT_WIDTH  = INPUT_WIDTH - FILTER_SIZE + 1;
+    localparam ROW_START     = FILTER_SIZE/2;
+    localparam ROW_END       = INPUT_HEIGHT - FILTER_SIZE/2 - 1;
+    localparam COL_START     = FILTER_SIZE/2;
+    localparam COL_END       = INPUT_WIDTH - FILTER_SIZE/2 - 1;
     
     // Initialize trainable parameters
-    // Weights
-    (* rom_style = "block" *) logic signed [7:0]
-    weights [NUM_FILTERS-1:0][OUTPUT_WIDTH-1:0][OUTPUT_HEIGHT-1:0];
-    initial $readmemb(WEIGHTS_FILE, weights);
-    // Biases
-    (* rom_style = "block" *) logic signed [7:0]
-    biases [NUM_FILTERS-1:0][OUTPUT_WIDTH-1:0][OUTPUT_HEIGHT-1:0];
+    // 3 S2 map connections weights
+    (* rom_style = "block" *) logic signed [15:0]
+    weights3map [5:0][2:0][FILTER_SIZE-1:0][FILTER_SIZE-1:0];
+    initial $readmemb(WEIGHTS_FILE3MAP, weights);
+    // 4 S2 map connections weights
+    (* rom_style = "block" *) logic signed [15:0]
+    weights4map [8:0][3:0][FILTER_SIZE-1:0][FILTER_SIZE-1:0];
+    initial $readmemb(WEIGHTS_FILE4MAP, weights);
+    // 6 S2 map connections weights
+    (* rom_style = "block" *) logic signed [15:0]
+    weights6map [5:0][FILTER_SIZE-1:0][FILTER_SIZE-1:0];
+    initial $readmemb(WEIGHTS_FILE6MAP, weights);
+    // Biases (1 bias per C3 feature map)
+    (* rom_style = "block" *) logic signed [15:0]
+    biases [NUM_FILTERS-1:0];
     initial $readmemb(BIASES_FILE, biases);
-
-    // For height=5 filter, we only need to store 4 rows of pixel data
-    // We could reduce latency if we get creative with the fill order of the LB
-    logic        [7:0] line_buffer[FILTER_SIZE-2:0][INPUT_WIDTH-1:0];
-    
-    // Window is pixel block to be element-wise multiplied with filter kernel (5x5 for conv1 of LeNet-5)
-    logic        [7:0] window[FILTER_SIZE-1:0][FILTER_SIZE-1:0];
-    // Indexed window, weight value to be used for * operation
-    logic        [7:0] window_value;
-    // Weights are signed
-    logic signed [7:0] weight_value;
-    
-    // control counters
-    logic [$clog2(INPUT_HEIGHT)-1:0] row_ctr;
-    logic [$clog2(INPUT_WIDTH)-1:0]  col_ctr;
-    logic [$clog2(NUM_FILTERS)-1:0]  filter_ctr;
-    logic [$clog2(WINDOW_AREA)-1:0]  mac_ctr;
-        
-    // MACC accumulate
-    logic signed [15:0] mac_accum;
-    
-    typedef enum logic {
-        IDLE, MACC
-    } state;
-    state next_state, curr_state;
-    
-    always_ff @(posedge i_clk or negedge i_rst)
-        if (~i_rst)
-            curr_state <= IDLE;
-        else
-            curr_state <= next_state;
-    
-    // Next state logic
-    always_comb begin
-        case (curr_state)
-            IDLE:
-                if (i_feature_valid) next_state <= MACC;
-            MACC:
-                // Do we want the "-1" on the counters?
-                if (filter_ctr == NUM_FILTERS-1 &&
-                    row_ctr == INPUT_HEIGHT-1 &&
-                    col_ctr == INPUT_WIDTH-1)
-                        next_state <= IDLE;
-                        
-            default: next_state <= IDLE;
-        endcase
-    end
-        
-    // Wasted resources having async resets and sync resets (in IDLE)?
-    // Limit to sync reset?
-    
-    // Data Out is probably a wasted state
-    always_ff @(posedge i_clk or negedge i_rst) begin
-        if (~i_rst) begin
-            row_ctr    <='b0;
-            col_ctr    <='b0;
-            filter_ctr <='b0;
-            mac_ctr    <='b0;
-            mac_accum  <='b0;
-        end else begin
-            // Biases is first added to accumulate for efficiency
-            // While we are in the MACC state, the weight*feature operations are added
-            mac_accum    <= biases[filter_ctr][row_ctr][col_ctr];
-            window_value <= window[mac_ctr/FILTER_SIZE][mac_ctr%FILTER_SIZE];
-            // Need to simplify this! Maybe weights should also be a 2D SR
-            weight_value <= weights[filter_ctr]
-                                   [row_ctr - FILTER_SIZE/2 + mac_ctr/FILTER_SIZE]
-                                   [col_ctr - FILTER_SIZE/2 + mac_ctr%FILTER_SIZE];
-            case (curr_state)
-                IDLE: begin
-                    if (i_feature_valid) begin
-                        row_ctr         <=  'b0;
-                        col_ctr         <=  'b0;
-                        filter_ctr      <=  'b0;
-                        mac_ctr         <=  'b0;
-                        window          <= '{default: 0};
-                        line_buffer     <= '{default: 0};
-                        mac_accum       <= biases[0][0][0];
-                        o_feature_valid <= 1'b0;
-                    end
-                end
-                MACC: begin
-                    mac_ctr <= mac_ctr == WINDOW_AREA-1 ? 'b0 : mac_ctr + 1'b1;
-                    o_feature_valid <= 1'b0;
-                    mac_accum <= mac_accum + window_value * weight_value;
-                    if (mac_ctr == WINDOW_AREA-1) begin
-                        o_feature_valid <= 1'b1;
-                        filter_ctr      <= filter_ctr == NUM_FILTERS-1 ? 'b0 : filter_ctr + 1'b1;
-                    end
-                end
-            endcase
-        end
-    end
-    
-    always_ff @(posedge i_clk) begin
-        // Generate window by shifting right by 1
-        for (int i = 0; i < FILTER_SIZE; i++)
-            for (int j = 0; j < FILTER_SIZE-1; j++)
-                window[i][j] <= window[i][j+1];
-        
-        // Last column of pixel window assigned values from equivalent column of line buffer
-        // Check synthesis here, is there simply FILTER_SIZE-1 SRLs?
-        for (int i = 0; i < FILTER_SIZE-1; i++)
-            window[i][FILTER_SIZE-1] <= line_buffer[i][col_ctr];
-        
-        // The bottom right corner of the pixel window, will take the incoming pixel as its value
-        window[FILTER_SIZE-1][FILTER_SIZE-1] <= i_feature;
-        
-        // Line buffer
-        for (int i = 0; i < FILTER_SIZE-2; i++)
-            line_buffer[i][col_ctr] <= line_buffer[i+1][col_ctr];
-        line_buffer[FILTER_SIZE-2][col_ctr]   <= i_feature;
-        line_buffer[0][col_ctr-FILTER_SIZE+1] <= window[FILTER_SIZE-1][col_ctr-FILTER_SIZE+1];
-        
-        // Row/Column counters
-        col_ctr <= col_ctr + 1'b1;
-        if (col_ctr == INPUT_WIDTH-1) begin
-            col_ctr <= 'b0;
-            // Can always incr row cnt because it gets reset when
-            // we fall back into the IDLE state of the main FSM
-            row_ctr <= row_ctr + 1'b1;
-        end
-    end
-    
-    assign o_feature = mac_accum;
 
 endmodule
