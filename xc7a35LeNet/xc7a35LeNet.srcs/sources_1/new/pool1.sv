c3a173a7db20107bbe188d94034b33341d3c897d
steckleindesign <steckleindesign@gmail.com>
2025-03-02 00:36:33 -0500

    max pool layer updated, not tested

--- xc7a35LeNet/xc7a35LeNet.srcs/sources_1/new/pool1.sv
+++ xc7a35LeNet/xc7a35LeNet.srcs/sources_1/new/pool1.sv
@@ -1,66 +1,62 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 
-/*
-Registers
-    row buffer
-    
-    
-
-
-1) Consume 1 row
-    Every second feature
-        compare that and the previous adjacent value
-            if 2nd value is max, then replace with max value
-2) Consume second row
-    every first feature
-        if feature in is greater than column above,
-            replace the row buf column with the feature
-    every 2nd feature consumed, compute the max
-    
-    
-    
-
-*/
-
 //////////////////////////////////////////////////////////////////////////////////
 
 module pool1(
     input  logic               clk,
     input  logic               rst,
-    input  logic               valid,
-    input  logic signed [15:0] features[0:5],
-    output logic               feature_out
+    input  logic               valid_in,
+    input  logic signed [15:0] features_in[0:5],
+    output logic               valid_out,
+    output logic               features_out[0:5]
 );
-    
-    logic first_row;
-    logic [4:0] col;
 
-    logic signed [15:0] row_buf [0:27];
-    initial row_buf = '{default: 0};
-    logic 
+    localparam NUM_FILTERS       = 6;
+    localparam FEATURE_MAP_WIDTH = 28;
+
+    logic row, col;
+    logic [$clog2(FEATURE_MAP_WIDTH/2):0] cnt;
+    logic signed [15:0] row_buf [0:NUM_FILTERS-1][0:FEATURE_MAP_WIDTH/2-1];
     
     always_ff @(clk)
-    begin
         if (rst)
         begin
-            first_row <= 1;
-            col       <= 0;
+            row <= 0;
+            col <= 0;
+            cnt <= 0;
+            row_buf <= '{default: 0};
+            valid_out <= 0;
         end
         else
         begin
-            if (valid)
+            valid_out <= 0;
+            if (valid_in)
             begin
-                col <= col + 1;
-                if (col == 5'd26)
+                col <= ~col;
+                cnt <= cnt + 1;
+                if (cnt == FEATURE_MAP_WIDTH/2 - 1)
                 begin
-                    col       <= 0;
-                    first_row <= 0;
-                    row_buf[0] <= row_buf[1];
+                    cnt <= 0;
+                    row <= ~row;
                 end
-                row_buf[!first_row][col] <= features;
+                if (~row & ~col)
+                    for (int i = 0; i < NUM_FILTERS; i++)
+                        row_buf[i][cnt] <= features_in[i];
+                else
+                begin
+                    for (int i = 0; i < NUM_FILTERS; i++)
+                        if (features_in[i] > row_buf[i][cnt])
+                        begin
+                            row_buf[i][cnt] <= features_in[i];
+                            features_out[i] <= features_in[i];
+                        end
+                        else
+                            features_out[i] <= row_buf[i][cnt];
+                    if (row & col)
+                        valid_out <= 1;
+                end
             end
         end
-    end
 
 endmodule
